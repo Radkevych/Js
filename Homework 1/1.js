ed5b68184c2216974b27fb41caa42ad12ac8a952
Oleksandr Radkevych <118602704+Radkevych@users.noreply.github.com>
2023-02-15 16:05:38 +0200

    Add files via upload

---
+++ Homework 1/1.js
@@ -0,0 +1,9 @@
+for(let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log('Fiz')
+    } else if (i % 2 === 1) {
+        console.log('Buz')
+    } else if (i % 3 === 3) {
+        console.log ('FizBuz')
+    }
+}
